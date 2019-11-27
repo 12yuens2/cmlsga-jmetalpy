18a3574376663ccf75379358967f2dd40fe725a1
12yuens2 <sizhe1007@gmail.com>
2019-11-27 13:34:24 +0000

    Convert script to use experiments

--- script.py
+++ script.py
@@ -1,11 +1,15 @@
 from jmetal.algorithm.multiobjective.nsgaii import NSGAII
 from jmetal.algorithm.multiobjective.moead import MOEAD
 from jmetal.algorithm.multiobjective.omopso import OMOPSO
+from jmetal.config import store
+from jmetal.core.quality_indicator import *
 from jmetal.lab.experiment import Experiment, Job, generate_summary_from_experiment
 from jmetal.lab.visualization import Plot
-from jmetal.operator import PolynomialMutation, UniformMutation, DifferentialEvolutionCrossover, SBXCrossover
+from jmetal.operator import PolynomialMutation, UniformMutation, \
+    DifferentialEvolutionCrossover, SBXCrossover
 from jmetal.operator.mutation import NonUniformMutation
 from jmetal.problem.multiobjective.constrained import Srinivas
+from jmetal.problem.multiobjective.zdt import ZDT1, ZDT2
 from jmetal.util.aggregative_function import Tschebycheff
 from jmetal.util.archive import CrowdingDistanceArchive
 from jmetal.util.solutions import read_solutions
@@ -23,7 +27,7 @@ def nsgaii(problem, population_size, max_evaluations, evaluator):
                 probability=1.0 / problem.number_of_variables,
                 distribution_index=20
             ),
-            "crossover": SBXCrossover(probability=1.0, distribution_index=5),
+            "crossover": SBXCrossover(probability=1.0, distribution_index=2),
             "termination_criterion": StoppingByEvaluations(max=max_evaluations),
             "population_evaluator": evaluator
         }
@@ -73,23 +77,63 @@ def omopso(problem, population_size, max_evaluations, evaluator):
     )
 
 
+def configure_experiment(problems, num_runs):
+    jobs = []
+    max_evaluations = 15000
+    population_size = 100
+
+    for run in range(num_runs):
+        for problem in problems:
+            nsgaii_constructor, kwargs = nsgaii(problem, population_size,
+                                    max_evaluations, store.default_evaluator)
+            jobs.append(Job(
+                algorithm=nsgaii_constructor(**kwargs),
+                algorithm_tag="NSGAII",
+                problem_tag=problem.get_name(),
+                run=run
+            ))
+
+            moead_constructor, kwargs = moead(problem, population_size,
+                                  max_evaluations, store.default_evaluator)
+            jobs.append(Job(
+                algorithm=moead_constructor(**kwargs),
+                algorithm_tag="MOEAD",
+                problem_tag=problem.get_name(),
+                run=run
+            ))
+
+    return jobs
+
+
+
 if __name__ == "__main__":
-    problem = Srinivas()
-    problem.reference_front = read_solutions(filename="resources/reference_front/Srinivas.pf")
 
-    max_evaluations = 10000
+    jobs = configure_experiment([Srinivas(), ZDT1(), ZDT2()], 30)
 
-    mls = MultiLevelSelection(
-        max_evaluations=max_evaluations,
-        problem=problem,
-        algorithms=[nsgaii, moead, omopso]
+    output_directory = "data"
+
+    #experiment = Experiment(output_directory, jobs)
+    #experiment.run()
+
+    generate_summary_from_experiment(
+        output_directory,
+        [InvertedGenerationalDistance(), HyperVolume([1.0, 1.0])],
+        reference_fronts="resources/reference_front",
     )
-    mls.run()
 
-    #a, b = omopso(problem, 200, max_evaluations)
-    #algo = a(**b)
-    #algo.run()
-    front = mls.get_result()
+    #problem = Srinivas()
+    #problem.reference_front = read_solutions(filename="resources/reference_front/Srinivas.pf")
 
-    plot_front = Plot(plot_title='Pareto front approximation', reference_front=problem.reference_front, axis_labels=problem.obj_labels)
-    plot_front.plot(front, label="MLS", filename="MLS")
+    #max_evaluations = 10000
+
+    #mls = MultiLevelSelection(
+    #    max_evaluations=max_evaluations,
+    #    problem=problem,
+    #    algorithms=[nsgaii, moead]#, omopso]
+    #)
+    #mls.run()
+
+    #front = mls.get_result()
+
+    #plot_front = Plot(plot_title='Pareto front approximation', reference_front=problem.reference_front, axis_labels=problem.obj_labels)
+    #plot_front.plot(front, label="MLS", filename="MLS")
