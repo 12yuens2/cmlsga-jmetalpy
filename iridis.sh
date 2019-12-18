f64be002da927f9b934c47d8a98793a2c8be42d3
Sizhe Yuen <sizhe1007@gmail.com>
2019-12-18 16:29:40 +0000

    Update iridis run script

--- iridis.sh
+++ iridis.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+#PBS -l walltime=10:00:00
+#PBS -l nodes=6:ppn=10
+
 # Script to run code on iridis
 
-POPULATION=$1
-EVALUATIONS=$2
-RUNS=$3
-COMMENT=$4
+#POPULATION=$1
+#EVALUATIONS=$2
+#RUNS=$3
+#COMMENT=$4
 
 cd $PBS_O_WORKDIR
 
 echo "Running $POPULATION population, $EVALUATIONS evaluations, $RUNS runs, $COMMENT"
 
-python src/run_experiment $POPULATION $EVALUATIONS $RUNS $COMMENT
+module load conda/4.4.0
+source env/bin/activate
+python src/run_experiment.py $POPULATION $EVALUATIONS $RUNS $COMMENT
