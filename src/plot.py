54fd7aec0b35f3726618847f5d19a50f4e40f251
12yuens2 <sizhe1007@gmail.com>
2020-01-14 11:12:17 +0000

    Refactor plotting

--- src/plot.py
+++ src/plot.py
@@ -1,5 +1,6 @@
 from functools import partial
 
+from jmetal.problem.multiobjective.dtlz import *
 from jmetal.problem.multiobjective.zdt import *
 from jmetal.problem.multiobjective.lz09 import *
 from jmetal.lab.visualization import Plot
@@ -10,23 +11,29 @@ from cmlsga.algorithms.genetic_algorithms import *
 from cmlsga.algorithms.particle_swarm_optimisation import *
 
 
-def run_algorithm(algorithm, problem=ZDT1(), population_size=600,
-                  max_evaluations=10000, evaluator=store.default_evaluator):
+def run_algorithm(algorithm, problem=LZ09_F3(), population_size=600,
+                  max_evaluations=100000, evaluator=store.default_evaluator):
 
     constructor, kwargs = algorithm(problem, population_size, max_evaluations, evaluator)
     algorithm = constructor(**kwargs)
     algorithm.observable.register(ProgressBarObserver(max=max_evaluations))
     algorithm.run()
 
-    return algorithm.get_result()
+    return algorithm.get_result(), algorithm.get_name()
 
-reference_front = read_solutions(filename="resources/reference_front/ZDT1.pf")
-nsgaii_front = run_algorithm(nsgaii)
-moead_front = run_algorithm(moead)
-omopso_front = run_algorithm(omopso)
-mlsga_front = run_algorithm(partial(mlsga, [nsgaii]))
+def run_plots(algorithms, problems):
+    for problem in problems:
+        print("Run {}".format(problem.get_name()))
 
-plot_front = Plot(reference_front=reference_front, axis_labels=["x", "y"])
-plot_front.plot([nsgaii_front, moead_front, omopso_front, mlsga_front],
-                label=["NSGAII", "MOEAD", "OMOPSO", "MLSGA"])
+        reference_front = read_solutions(
+            filename="resources/reference_front/{}.pf".format(problem.get_name()))
 
+        fronts, labels = zip(*[run_algorithm(algorithm, problem=problem) for algorithm in algorithms])
+
+        plot_front = Plot(plot_title="{} 100000 evaluations".format(problem.get_name()),
+                          reference_front=reference_front, axis_labels=["x", "y"])
+        plot_front.plot(list(fronts), label=list(labels))
+
+
+run_plots([nsgaii, moead, omopso, partial(mlsga, [nsgaii])],
+          [ZDT1(), DTLZ1()])
