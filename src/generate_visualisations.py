0e685ec9209b28d32a8778b48696dbf671532b94
12yuens2 <sizhe1007@gmail.com>
2020-01-16 14:55:03 +0000

    Move generate summary to visualisations script

--- src/generate_visualisations.py
+++ src/generate_visualisations.py
@@ -1,5 +1,6 @@
 import sys
 
+from jmetal.core.quality_indicator import *
 from jmetal.lab.experiment import *
 
 def print_mean(datafile, indicator_name):
@@ -9,13 +10,21 @@ def print_mean(datafile, indicator_name):
 
 if __name__ == "__main__":
 
-    datafiles = sys.argv[1:]
+    data_directories = sys.argv[1:]
 
-    for data in datafiles:
+    for data in data_directories:
         print(data)
+        generate_summary_from_experiment(
+            data,
+            [InvertedGenerationalDistance(), HyperVolume([1.0, 1.0])],
+            reference_fronts="resources/reference_front"
+        )
 
-        print_mean(data, "IGD")
-        print_mean(data, "HV")
+        filename = data[5:-1]
+        os.rename("QualityIndicatorSummary.csv", filename)
+
+        print_mean(filename, "IGD")
+        print_mean(filename, "HV")
 
     #generate_latex_tables(filename=datafile)
 
