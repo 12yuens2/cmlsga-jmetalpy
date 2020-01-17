16cda105f65b459456fb755650d7ad88d60e9fa0
12yuens2 <sizhe1007@gmail.com>
2020-01-17 15:07:20 +0000

    Compute any stat (not just avg)

--- src/generate_visualisations.py
+++ src/generate_visualisations.py
@@ -1,12 +1,48 @@
 import sys
+import pandas as pd
 
 from jmetal.core.quality_indicator import *
 from jmetal.lab.experiment import *
 
-def print_mean(datafile, indicator_name):
-    avg = compute_mean_indicator(filename=datafile, indicator_name=indicator_name)
-    print("{} avg:".format(indicator_name))
-    print("{} \n".format(avg))
+def compute_indicator(filename, indicator_name, stat_function):
+    """
+    Adapted from jmetal.lab.experiment.compute_mean_indicator
+    Changed so that it takes an statistic function instead of just the mean
+    """
+    df = pd.read_csv(filename, skipinitialspace=True)
+
+    if len(set(df.columns.tolist())) != 5:
+        raise Exception('Wrong number of columns')
+
+    algorithms = pd.unique(df['Algorithm'])
+    problems = pd.unique(df['Problem'])
+
+    # We consider the quality indicator indicator_name
+    data = df[df['IndicatorName'] == indicator_name]
+
+    # Compute for each pair algorithm/problem the average of IndicatorValue
+    average_values = np.zeros((problems.size, algorithms.size))
+    j = 0
+    for alg in algorithms:
+        i = 0
+        for pr in problems:
+            average_values[i, j] = getattr(data['IndicatorValue'][np.logical_and(
+                data['Algorithm'] == alg, data['Problem'] == pr)], stat_function)()
+            i += 1
+        j += 1
+
+    # Generate dataFrame from average values and order columns by name
+    df = pd.DataFrame(data=average_values, index=problems, columns=algorithms)
+    df = df.reindex(df.columns, axis=1)
+
+    return df
+
+
+def print_stat(datafile, indicator_name, stat_function):
+    stat = compute_indicator(datafile, indicator_name, stat_function)
+    print("{} {}:".format(indicator_name, stat_function))
+    print("{} \n".format(stat))
+
 
 if __name__ == "__main__":
 
@@ -14,17 +50,18 @@ if __name__ == "__main__":
 
     for data in data_directories:
         print(data)
-        generate_summary_from_experiment(
-            data,
-            [InvertedGenerationalDistance(), HyperVolume([1.0, 1.0])],
-            reference_fronts="resources/reference_front"
-        )
+        #generate_summary_from_experiment(
+        #    data,
+        #    [InvertedGenerationalDistance(), HyperVolume([1.0, 1.0])],
+        #    reference_fronts="resources/reference_front"
+        #)
 
-        filename = data[5:-1]
-        os.rename("QualityIndicatorSummary.csv", filename)
+        #filename = data[5:-1]
+        #os.rename("QualityIndicatorSummary.csv", filename)
+        filename = data
 
         print_mean(filename, "IGD")
-        print_mean(filename, "HV")
+        #print_mean(filename, "HV")
 
     #generate_latex_tables(filename=datafile)
 
