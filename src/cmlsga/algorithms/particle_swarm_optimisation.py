bf7649ed198233facee1b634be2c17dacbcb1cb4
12yuens2 <sizhe1007@gmail.com>
2020-02-06 17:14:21 +0000

    Start on PSO variants to change velocity

--- src/cmlsga/algorithms/particle_swarm_optimisation.py
+++ src/cmlsga/algorithms/particle_swarm_optimisation.py
@@ -5,10 +5,60 @@ from jmetal.operator.mutation import NonUniformMutation
 from jmetal.util.archive import CrowdingDistanceArchive
 from jmetal.util.termination_criterion import StoppingByEvaluations
 
+class OMOPSO_Variant(OMOPSO):
+
+    def __init__(self, problem, swarm_size):
+        super(OMOPSO_Variant, self).__init__(problem, swarm_size)
+
+
+    def update_velocity(self, swarm):
+        for i in range(self.swarm_size):
+            pbest = copy(swarm[i].attributes['local_best'])
+            gbest = self.select_global_best()
+
+            r1 = round(random.uniform(self.r1_min, self.r1_max), 1)
+            r2 = round(random.uniform(self.r2_min, self.r2_max), 1)
+            c1 = round(random.uniform(self.c1_min, self.c1_max), 1)
+            c2 = round(random.uniform(self.c2_min, self.c2_max), 1)
+
+            w = round(random.uniform(self.weight_min, self.weight_max), 1)
+
+            for var in range(swarm[i].number_of_variables):
+                swarm_var = swarm[i].variables[var]
+                self.speed[i][var] = w * self.speed[i][var] \
+                                       + (c1 * r1 * (pbest.variables[var] - swarm_var)) \
+                                       + (c2 * r2 * (gbest.variables[var] - swarm_var))
+
+
+
+class SMPSO_Variant(SMPSO):
+
+    def __init__(self, problem, swarm_size):
+        super(SMPSO_Variant, self).__init__(problem, swarm_size)
+
+
+    def update_velocity(self, swarm):
+        for i in range(self.swarm_size):
+            pbest = copy(swarm[i].attributes['local_best'])
+            gbest = self.select_global_best()
+
+            r1 = round(random.uniform(self.r1_min, self.r1_max), 1)
+            r2 = round(random.uniform(self.r2_min, self.r2_max), 1)
+            c1 = round(random.uniform(self.c1_min, self.c1_max), 1)
+            c2 = round(random.uniform(self.c2_min, self.c2_max), 1)
+
+            w = round(random.uniform(self.weight_min, self.weight_max), 1)
+
+            for var in range(swarm[i].number_of_variables):
+                swarm_var = swarm[i].variables[var]
+                self.speed[i][var] = w * self.speed[i][var] \
+                                       + (c1 * r1 * (pbest.variables[var] - swarm_var)) \
+                                       + (c2 * r2 * (gbest.variables[var] - swarm_var))
+
 
 def omopso(problem, population_size, max_evaluations, evaluator):
     return (
-        OMOPSO,
+        OMOPSO_Variant,
         {
             "problem": problem,
             "swarm_size": population_size,
@@ -31,7 +81,7 @@ def omopso(problem, population_size, max_evaluations, evaluator):
 
 def smpso(problem, population_size, max_evaluations, evaluator):
     return (
-        SMPSO,
+        SMPSO_Variant,
         {
             "problem": problem,
             "swarm_size": population_size,
