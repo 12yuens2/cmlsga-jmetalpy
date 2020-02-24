3fc6d93f0763ed2d04a0a2c61a98ebfbc1883de9
Sizhe Yuen <sizhe1007@gmail.com>
2020-02-24 13:18:42 +0000

    Remove cache/dot files

--- env/lib/python3.6/site-packages/jmetal/algorithm/singleobjective/__init__.py
+++
@@ -1,4 +0,0 @@
-from .evolution_strategy import EvolutionStrategy
-from .genetic_algorithm import GeneticAlgorithm
-from .local_search import LocalSearch
-from .simulated_annealing import SimulatedAnnealing
