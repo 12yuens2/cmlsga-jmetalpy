397285349e2eb7334c3f16e3463e664bb671281e
Sizhe Yuen <sizhe1007@gmail.com>
2020-08-10 21:52:08 +0100

    Add MOP reference front generation

--- src/cmlsga/problems/mop.py
+++ src/cmlsga/problems/mop.py
@@ -1,3 +1,4 @@
+import numpy as np
 import math
 
 from jmetal.core.problem import FloatProblem
@@ -34,6 +35,12 @@ class MOP1(FloatProblem):
         solution.objectives[1] = (1 + g) * (1 - math.sqrt(x[0]))
 
 
+    def pf(self, obj, num_points):
+        f1 = [0 + i * 1/(num_points - 1) for i in range(num_points)]
+        f2 = [1 - math.sqrt(i) for i in f1]
+
+        return zip(f1, f2)
+
     def get_name(self):
         return "MOP1"
 
@@ -57,6 +64,12 @@ class MOP2(MOP1):
         solution.objectives[1] = (1 + g) * (1 - x[0] * x[0])
 
 
+    def pf(self, obj, num_points):
+        f1 = [0 + i * 1/(num_points - 1) for i in range(num_points)]
+        f2 = [1 - i * i for i in f1]
+
+        return zip(f1, f2)
+
     def get_name(self):
         return "MOP2"
 
@@ -77,6 +90,13 @@ class MOP3(MOP1):
 
         solution.objectives[0] = (1 + g) * math.cos(x[0] * math.pi * 0.5)
         solution.objectives[1] = (1 + g) * math.sin(x[0] * math.pi * 0.5)
+
+
+    def pf(self, obj, num_points):
+        f1 = [0 + i * 1/(num_points - 1) for i in range(num_points)]
+        f2 = [math.sqrt(1 - i * i) for i in f1]
+
+        return zip(f1, f2)
 
 
     def get_name(self):
@@ -102,6 +122,22 @@ class MOP4(MOP1):
         solution.objectives[1] = (1 + g) * (1 - math.sqrt(x[0]) * math.pow(math.cos(x[0] * math.pi * 2), 2))
 
 
+    def pf(self, obj, num_points):
+        min_val = 300
+        f1 = []
+        f2 = []
+        for i in range(num_points):
+            v1 = 0 + i * 1/(num_points - 1)
+            v2 = 1 - math.sqrt(v1) * math.cos(2 * math.pi * v1 * v1)
+
+            if v2 < min_val:
+                f1.append(v1)
+                f2.append(v2)
+                min_val = v2
+
+        return zip(f1, f2)
+
+
     def get_name(self):
         return "MOP4"
 
@@ -122,6 +158,13 @@ class MOP5(MOP1):
 
         solution.objectives[0] = (1 + g) * x[0]
         solution.objectives[1] = (1 + g) * (1 - math.sqrt(x[0]))
+
+
+    def pf(self, obj, num_points):
+        f1 = [0 + i * 1/(num_points - 1) for i in range(num_points)]
+        f2 = [1 - math.sqrt(i) for i in f1]
+
+        return zip(f1, f2)
 
 
     def get_name(self):
@@ -153,6 +196,18 @@ class MOP6(MOP1):
         solution.objectives[2] = (1 + g) * (1 - x[0])
 
 
+    def pf(self, obj, num_points):
+        points = []
+        for i in range(num_points):
+            # Generate random points, then normalise
+            vec = np.random.uniform(0, 1, 3)
+            arr = vec / np.sum(vec)
+
+            points.append(tuple(arr))
+
+        return points
+
+
     def get_name(self):
         return "MOP6"
 
@@ -174,6 +229,24 @@ class MOP7(MOP6):
         solution.objectives[0] = (1 + g) * math.cos(0.5 * math.pi * x[0]) * math.cos(0.5 * math.pi * x[1])
         solution.objectives[1] = (1 + g) * math.cos(0.5 * math.pi * x[0]) * math.sin(0.5 * math.pi * x[1])
         solution.objectives[2] = (1 + g) * math.sin(0.5 * math.pi * x[0])
+
+
+    def pf(self, obj, num_points):
+        #todo refactor uniform point generation into a mop6 function
+        points = []
+        for i in range(num_points):
+            vec = np.random.uniform(0, 1, 3)
+            arr = vec / np.sum(vec)
+
+            points.append(arr)
+
+        norm_points = []
+        for arr in points:
+            s = math.sqrt(sum([i * i for i in arr]))
+            norm = [i / s for i in arr]
+            norm_points.append(tuple(norm))
+
+        return norm_points
 
 
     def get_name(self):
