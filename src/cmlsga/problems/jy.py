a08e8fc41f13d4da02c2d63d57bdef210e388046
Sizhe Yuen <sizhe1007@gmail.com>
2022-05-19 17:47:17 +0100

    Use lambdify and add jy differentials

--- src/cmlsga/problems/jy.py
+++ src/cmlsga/problems/jy.py
@@ -5,6 +5,8 @@ import numpy as np
 from jmetal.core.problem import FloatProblem, DynamicProblem
 from jmetal.core.solution import FloatSolution
 
+from sympy import sin, pi, cos, floor
+
 
 class JY(DynamicProblem, FloatProblem):
     def __init__(self):
@@ -39,7 +41,11 @@ class JY(DynamicProblem, FloatProblem):
         self.problem_modified = False
 
     def evaluate(self, solution):
-        pass
+        x = solution.variables
+        f0, f1 = self.eval_objectives(x, self.get_at(), self.get_wt())
+
+        solution.objectives[0] = f0
+        solution.objectives[1] = f1
 
     def eval_objectives(self, x, at, wt):
         sum_gx = sum([math.pow(x[i] - self.gt, 2) for i in range(1, self.number_of_variables)])
@@ -49,6 +55,21 @@ class JY(DynamicProblem, FloatProblem):
 
         return obj1, obj2
 
+    def get_differentials(self, s, at, wt):
+        sum_gx = sum([(s.xvars[i] - self.gt) ** 2 for i in range(1, self.number_of_variables)])
+
+        f0 = (1 + sum_gx) * (s.xvars[0] + at * sin(wt * pi * s.xvars[0]))
+        f1 = (1 + sum_gx) * (1 - s.xvars[0] + at * sin(wt * pi * s.xvars[0]))
+
+        return (f0, f1)
+
+
+    def get_at(self):
+        pass
+
+    def get_wt(self):
+        pass
+
     def get_name(self):
         pass
 
@@ -57,38 +78,37 @@ class JY1(JY):
     def __init__(self):
         super().__init__()
 
-    def evaluate(self, solution):
-        x = solution.variables
-        at = 0.05
-        wt = 6
+    def get_at(self):
+        return 0.05
 
-        solution.objectives[0], solution.objectives[1] = self.eval_objectives(x, at, wt)
+    def get_wt(self):
+        return 6
 
+    def differentials(self, s):
+        return self.get_differentials(s, self.get_at(), self.get_wt())
 
     def pf(self, obj, num_points, time):
         pass
 
-
     def get_name(self):
         return "JY1"
-
 
 
 class JY2(JY):
     def __init__(self):
         super().__init__()
 
-    def evaluate(self, solution):
-        x = solution.variables
-        at = 0.05
-        wt = math.floor(6 * math.sin(0.5 * math.pi * (self.time - 1)))
+    def get_at(self):
+        return 0.05
 
-        solution.objectives[0], solution.objectives[1] = self.eval_objectives(x, at, wt)
+    def get_wt(self):
+        return math.floor(6 * math.sin(0.5 * math.pi * (self.time - 1)))
 
-
+    def differentials(self, s):
+        return self.get_differentials(s, self.get_at(), self.get_wt())
+    
     def pf(self, obj, num_points, time):
         pass
-
 
     def get_name(self):
         return "JY2"
@@ -98,13 +118,11 @@ class JY3(JY):
     def __init__(self):
         super().__init__()
 
+        self.at = 0.05
 
     def evaluate(self, solution):
         x = solution.variables
-        at = 0.05
         wt = math.floor(6 * math.sin(0.5 * math.pi * (self.time - 1)))
-
-
         alpha = math.floor(100 * math.pow(math.sin(0.5 * math.pi * self.time), 2))
         y0 = abs(x[0] * math.sin((2 * alpha + 0.5) * math.pi * x[0]))
 
@@ -119,9 +137,29 @@ class JY3(JY):
 
             sum_gx += math.pow(math.pow(yi, 2) - yj, 2)
 
+        solution.objectives[0] = (1 + sum_gx) * (y0 + self.at * math.sin(wt * math.pi * y0))
+        solution.objectives[1] = (1 + sum_gx) * (1 - y0 + self.at * math.sin(wt * math.pi * y0))
 
-        solution.objectives[0] = (1 + sum_gx) * (y0 + at * math.sin(wt * math.pi * y0))
-        solution.objectives[1] = (1 + sum_gx) * (1 - y0 + at * math.sin(wt * math.pi * y0))
+    def differentials(self, s):
+        wt = floor(6 * sin(0.5 * pi * (self.time - 1)))
+        alpha = floor(100 * (sin(0.5 * pi * self.time) ** 2))
+        y0 = abs(s.xvars[0] * sin((2 * alpha + 0.5) * pi * s.xvars[0]))
+
+        sum_gx = 0
+        for i in range(1, self.number_of_variables):
+            yi = s.xvars[i]
+            yj = 0
+            if i == 1:
+                yj = y0
+            else:
+                yj = s.xvars[i - 1]
+
+            sum_gx += ((yi ** 2 ) - yj) ** 2
+
+        f0 = (1 + sum_gx) * (y0 + self.at * sin(wt * pi * y0))
+        f1 = (1 + sum_gx) * (1 - y0 + self.at * sin(wt * pi * y0))
+
+        return (f0, f1)
 
 
     def pf(self, obj, num_points, time):
@@ -137,18 +175,17 @@ class JY4(JY):
     def __init__(self):
         super().__init__()
 
+    def get_at(self):
+        return 0.05
 
-    def evaluate(self, solution):
-        x = solution.variables
-        at = 0.05
-        wt = math.pow(10, 1 + abs(self.gt))
+    def get_wt(self):
+        return math.pow(10, 1 + abs(self.gt))
 
-        solution.objectives[0], solution.objectives[1] = self.eval_objectives(x, at, wt)
-
+    def differentials(self, s):
+        return self.get_differentials(s, self.get_at(), self.get_wt())
 
     def pf(self, obj, num_points, time):
         pass
-
 
     def get_name(self):
         return "JY4"
@@ -159,18 +196,17 @@ class JY5(JY):
     def __init__(self):
         super().__init__()
 
+    def get_at(self):
+        return 0.3 * math.sin(0.5 * math.pi * (self.time - 1))
 
-    def evaluate(self, solution):
-        x = solution.variables
-        at = 0.3 * math.sin(0.5 * math.pi * (self.time - 1))
-        wt = 1
+    def get_wt(self):
+        return 1
 
-        solution.objectives[0], solution.objectives[1] = self.eval_objectives(x, at, wt)
-
+    def differentials(self, s):
+        return self.get_differentials(s, self.get_at(), self.get_wt())
 
     def pf(self, obj, num_points, time):
         pass
-
 
     def get_name(self):
         return "JY5"
@@ -179,21 +215,32 @@ class JY5(JY):
 class JY6(JY):
     def __init__(self):
         super().__init__()
-
+        self.at = 0.1
+        self.wt = 3
 
     def evaluate(self, solution):
         x = solution.variables
-        at = 0.1
-        wt = 3
         kt = 2 * math.floor(10 * abs(self.gt))
-
         sum_gx = 0
         for i in range(1, self.number_of_variables):
             yi = x[i] - self.gt
             sum_gx += 4 * math.pow(yi, 2) - math.cos(kt * math.pi * yi) + 1
 
-        solution.objectives[0] = (1 + sum_gx) * (x[0] + at * math.sin(wt * math.pi * x[0]))
-        solution.objectives[1] = (1 + sum_gx) * (1 - x[0] + at * math.sin(wt * math.pi * x[0]))
+        solution.objectives[0] = (1 + sum_gx) * (x[0] + self.at * math.sin(self.wt * math.pi * x[0]))
+        solution.objectives[1] = (1 + sum_gx) * (1 - x[0] + self.at * math.sin(self.wt * math.pi * x[0]))
+
+
+    def differentials(self, s):
+        kt = 2 * math.floor(10 * abs(self.gt))
+        sum_gx = 0
+        for i in range(1, self.number_of_variables):
+            yi = s.xvars[i] - self.gt
+            sum_gx += 4 * (yi ** 2) - cos(kt * pi * yi) + 1
+
+        f0 = (1 + sum_gx) * (s.xvars[0] + self.at * sin(self.wt * pi * s.xvars[0]))
+        f1 = (1 + sum_gx) * (1 - s.xvars[0] + self.at * sin(self.wt * pi * s.xvars[0]))
+
+        return (f0, f1)
 
 
     def pf(self, obj, num_points, time):
@@ -207,12 +254,11 @@ class JY6(JY):
 class JY7(JY):
     def __init__(self):
         super().__init__()
-
+        self.at = 0.1
+        self.wt = 3
 
     def evaluate(self, solution):
         x = solution.variables
-        at = 0.1
-        wt = 3
         alpha_t = 0.2 + 2.8 * abs(self.gt)
 
         sum_gx = 0
@@ -220,8 +266,21 @@ class JY7(JY):
             yi = x[i] - self.gt
             sum_gx += math.pow(yi, 2) - 10 * math.cos(2 * math.pi * yi) + 10
 
-        solution.objectives[0] = (1 + sum_gx) * math.pow(x[0] + at * math.sin(wt * math.pi * x[0]), alpha_t)
-        solution.objectives[1] = (1 + sum_gx) * math.pow(1 - x[0] + at * math.sin(wt * math.pi * x[0]), alpha_t)
+        solution.objectives[0] = (1 + sum_gx) * math.pow(x[0] + self.at * math.sin(self.wt * math.pi * x[0]), alpha_t)
+        solution.objectives[1] = (1 + sum_gx) * math.pow(1 - x[0] + self.at * math.sin(self.wt * math.pi * x[0]), alpha_t)
+
+
+    def differentials(self, s):
+        alpha_t = 0.2 + 2.8 * abs(self.gt)
+        sum_gx = 0
+        for i in range(1, self.number_of_variables):
+            yi = s.xvars[i] - self.gt
+            sum_gx += (yi ** 2) - 10 * cos(2 * pi * yi) + 10
+
+        f0 = (1 + sum_gx) * ((s.xvars[0] + self.at * sin(self.wt * pi * s.xvars[0])) ** alpha_t)
+        f1 = (1 + sum_gx) * ((1 - s.xvars[0] + self.at * sin(self.wt * pi * s.xvars[0])) ** alpha_t)
+
+        return (f0, f1)
 
         
     def pf(self, obj, num_points, time):
@@ -236,19 +295,30 @@ class JY7(JY):
 class JY8(JY):
     def __init__(self):
         super().__init__()
-
+        self.at = 0.05
+        self.wt = 6
 
     def evaluate(self, solution):
         x = solution.variables
-        at = 0.05
-        wt = 6
         beta_t = 10 - 9.8 * abs(self.gt)
         alpha_t = 2 / beta_t
 
         sum_gx = sum([math.pow(x[i], 2) for i in range(1, self.number_of_variables)])
 
-        solution.objectives[0] = (1 + sum_gx) * math.pow(x[0] + at * math.sin(wt * math.pi * x[0]), alpha_t)
-        solution.objectives[1] = (1 + sum_gx) * math.pow(1 - (x[0] + at * math.sin(wt * math.pi * x[0])), alpha_t)
+        solution.objectives[0] = (1 + sum_gx) * math.pow(x[0] + self.at * math.sin(self.wt * math.pi * x[0]), alpha_t)
+        solution.objectives[1] = (1 + sum_gx) * math.pow(1 - (x[0] + self.at * math.sin(self.wt * math.pi * x[0])), alpha_t)
+
+
+    def differentials(self, s):
+        beta_t = 10 - 9.8 * abs(self.gt)
+        alpha_t = 2 / beta_t
+
+        sum_gx = sum([s.xvars[i] ** 2 for i in range(1, self.number_of_variables)])
+
+        f0 = (1 + sum_gx) * ((s.xvars[0] + self.at * sin(self.wt * pi * s.xvars[0])) ** alpha_t)
+        f1 = (1 + sum_gx) * ((1 - (s.xvars[0] + self.at * sin(self.wt * pi * s.xvars[0]))) ** alpha_t)
+
+        return (f0, f1)
 
 
     def pf(self, obj, num_points, time):
