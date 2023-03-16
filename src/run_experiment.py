4f13fd30a023b984cc8f3c38abcbac505e68493d
Sizhe Yuen <sizhe1007@gmail.com>
2023-03-16 17:16:02 +0000

    Update run_experiment, ga, and pso codes to remove or comment out smac artifacts

--- src/run_experiment.py
+++ src/run_experiment.py
@@ -129,53 +129,49 @@ def parse_problem(problem):
 
 if __name__ == "__main__":
 
-    if len(sys.argv) == 3:
-        #json_file = open(sys.argv[1], "r")
-        #parameters = json.loads(json_file.read())
+    json_file = open(sys.argv[1], "r")
+    parameters = json.loads(json_file.read())
 
-        #population_size = parameters["population_size"]
-        #max_evaluations = parameters["max_evaluations"]
-        #number_of_runs = parameters["number_of_runs"]
-        #comment = parameters["comment"]
+    population_size = parameters["population_size"]
+    max_evaluations = parameters["max_evaluations"]
+    number_of_runs = parameters["number_of_runs"]
+    comment = parameters["comment"]
 
-        #algorithms = parse_algorithms(parameters)
-        #problems = parse_problems(parameters)
+    algorithms = parse_algorithms(parameters)
+    problems = parse_problems(parameters)
 
-        #run_experiment(population_size, max_evaluations, number_of_runs, comment=comment,
-        #               algorithms=algorithms,
-        #               problems=problems)
+    run_experiment(population_size, max_evaluations, number_of_runs, comment=comment,
+                   algorithms=algorithms,
+                   problems=problems)
 
-        runs = 10
-        max_evaluations = 100000
-        output_path = "data-full-smac-100k"
+    #runs = 10
+    #max_evaluations = 100000
+    #output_path = "data-full-smac-100k"
 
-        algo_type = sys.argv[2]
+    #algo_type = sys.argv[2]
 
-        with open(sys.argv[1], "r") as smac_file:
-            reader = csv.reader(smac_file)
-            for line in reader:
-                algorithm = globals()[line[0]]
-                problem = parse_problem(line[1])
-                population_size = int(line[2])
-                crossover_rate = 0
-                mutation_rate = float(line[4])
-                leaders = 0
-                e = MapEvaluator(processes=4)
+    #with open(sys.argv[1], "r") as smac_file:
+    #    reader = csv.reader(smac_file)
+    #    for line in reader:
+    #        algorithm = globals()[line[0]]
+    #        problem = parse_problem(line[1])
+    #        population_size = int(line[2])
+    #        crossover_rate = 0
+    #        mutation_rate = float(line[4])
+    #        leaders = 0
+    #        e = MapEvaluator(processes=4)
 
-                if algo_type == "pso":
-                    leaders = int(line[5])
-                    crossover_rate = leaders
-                else:
-                    crossover_rate = float(line[3])
+    #        if algo_type == "pso":
+    #            leaders = int(line[5])
+    #            crossover_rate = leaders
+    #        else:
+    #            crossover_rate = float(line[3])
 
-                constructor, kwargs = algorithm(problem, population_size, max_evaluations,
-                                                e, mutation_rate, crossover_rate)
-                algorithm = constructor(**kwargs)
+    #        constructor, kwargs = algorithm(problem, population_size, max_evaluations,
+    #                                        e, mutation_rate, crossover_rate)
+    #        algorithm = constructor(**kwargs)
 
-                jobs = [Job(algorithm, algorithm.get_name(), problem.get_name(), run)
-                        for run in range(runs)]
-                experiment = Experiment(output_path, jobs)
-                experiment.run()
-
-    else:
-        print_usage()
+    #        jobs = [Job(algorithm, algorithm.get_name(), problem.get_name(), run)
+    #                for run in range(runs)]
+    #        experiment = Experiment(output_path, jobs)
+    #        experiment.run()
